d78da2dca63bafd43ded9eff459be3c11c1a0820
yotai023 <yotai023333@gmail.com>
2025-01-03 00:03:40 +0900

    会員側の認証を実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
             $table->string('name');
+            $table->string('kana');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('postal_code');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->date('birthday')->nullable();
+            $table->string('occupation')->nullable();
+            $table->rememberToken('remember_token');
             $table->timestamps();
         });
     }
