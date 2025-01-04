f9805d5738e5a1105957e88c262aeb2580dd530c
yotai023 <yotai023333@gmail.com>
2025-01-04 20:04:03 +0900

    ホームページ表示修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                     <a href="{{ route('restaurants.show', $highly_rated_restaurant) }}" class="link-dark nagoyameshi-card-link">
                         <div class="card h-100">
                             @if ($highly_rated_restaurant->image !== '')
-                                <img src="data:image/jpeg;base64, {{ $highly_rated_restaurant->image) }}" class="card-img-top nagoyameshi-vertical-card-image">
+                                <img src="data:image/jpeg;base64, {{ $highly_rated_restaurant->image }}" class="card-img-top nagoyameshi-vertical-card-image">
                             @else
                                 <img src="{{ asset('/images/no_image.jpg') }}" class="card-img-top nagoyameshi-vertical-card-image" alt="画像なし">
                             @endif
@@ -171,7 +171,7 @@
                     <a href="{{ route('restaurants.show', $new_restaurant) }}" class="link-dark nagoyameshi-card-link">
                         <div class="card h-100">
                             @if ($new_restaurant->image !== '')
-                                <img src="data:image/jpeg;base64, {{ $new_restaurant->image) }}" class="card-img-top nagoyameshi-vertical-card-image">
+                                <img src="data:image/jpeg;base64, {{ $new_restaurant->image }}" class="card-img-top nagoyameshi-vertical-card-image">
                             @else
                                 <img src="{{ asset('/images/no_image.jpg') }}" class="card-img-top nagoyameshi-vertical-card-image" alt="画像なし">
                             @endif
